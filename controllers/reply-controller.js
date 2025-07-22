c691cc1be22187397d42654c426ed865ba88f7a0
ryx13 <ryanmuxi13@gmail.com>
2025-07-22 06:33:54 +0200

    Initial commit

--- controllers/reply-controller.js
+++ controllers/reply-controller.js
@@ -1,60 +1,64 @@
 const Thread = require('../models/thread');
 
-exports.post = (req, res) => {
-  const { text, delete_password, thread_id } = req.body;
-  const newReply = {
-    text,
-    delete_password,
-    created_on: new Date(),
-    reported: false
-  };
-  Thread.findByIdAndUpdate(thread_id, {
-    $push: { replies: newReply },
-    $set: { bumped_on: new Date() }
-  }, (err, thread) => {
-    if (err) return res.status(500).send(err);
+exports.post = async (req, res) => {
+  try {
+    const { text, delete_password, thread_id } = req.body;
+    const newReply = {
+      text,
+      delete_password,
+      created_on: new Date(),
+      reported: false
+    };
+    const thread = await Thread.findByIdAndUpdate(thread_id, {
+      $push: { replies: newReply },
+      $set: { bumped_on: new Date() }
+    }, { new: true });
     if (!thread) return res.status(404).send('Thread not found');
     res.status(200).send(thread);
-  });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
-exports.get = (req, res) => {
-  Thread.findById(req.query.thread_id)
-    .populate('replies', 'text created_on')
-    .exec((err, thread) => {
-      if (err) return res.status(500).send(err);
-      if (!thread) return res.status(404).send('Thread not found');
-      res.status(200).send(thread);
-    });
+exports.get = async (req, res) => {
+  try {
+    const thread = await Thread.findById(req.query.thread_id)
+      .populate('replies', 'text created_on')
+      .exec();
+    if (!thread) return res.status(404).send('Thread not found');
+    res.status(200).send(thread);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
-exports.delete = (req, res) => {
-  const { thread_id, reply_id, delete_password } = req.body;
-  Thread.findById(thread_id, (err, thread) => {
-    if (err) return res.status(500).send(err);
+exports.delete = async (req, res) => {
+  try {
+    const { thread_id, reply_id, delete_password } = req.body;
+    const thread = await Thread.findById(thread_id);
     if (!thread) return res.status(404).send('Thread not found');
     const reply = thread.replies.id(reply_id);
     if (!reply) return res.status(404).send('Reply not found');
     if (reply.delete_password !== delete_password) return res.status(200).send('incorrect password');
     reply.text = '[deleted]';
-    thread.save((err) => {
-      if (err) return res.status(500).send(err);
-      res.status(200).send('success');
-    });
-  });
+    await thread.save();
+    res.status(200).send('success');
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
-exports.put = (req, res) => {
-  const { thread_id, reply_id } = req.body;
-  Thread.findById(thread_id, (err, thread) => {
-    if (err) return res.status(500).send(err);
+exports.put = async (req, res) => {
+  try {
+    const { thread_id, reply_id } = req.body;
+    const thread = await Thread.findById(thread_id);
     if (!thread) return res.status(404).send('Thread not found');
     const reply = thread.replies.id(reply_id);
     if (!reply) return res.status(404).send('Reply not found');
     reply.reported = true;
-    thread.save((err) => {
-      if (err) return res.status(500).send(err);
-      res.status(200).send('reported');
-    });
-  });
+    await thread.save();
+    res.status(200).send('reported');
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
--- controllers/thread-controller.js
+++ controllers/thread-controller.js
@@ -1,54 +1,60 @@
 const Thread = require('../models/thread');
 
-exports.post = (req, res) => {
-  const { text, delete_password, board } = req.body;
-  const newThread = new Thread({
-    board,
-    text,
-    delete_password,
-    created_on: new Date(),
-    bumped_on: new Date(),
-    reported: false,
-    replies: []
-  });
-  newThread.save((err, savedThread) => {
-    if (err) return res.status(500).send(err);
-    res.status(200).send(savedThread);
-  });
-};
-
-exports.get = (req, res) => {
-  Thread.find({ board: req.params.board })
-    .sort({ bumped_on: -1 })
-    .limit(10)
-    .populate('replies', 'text created_on')
-    .exec((err, threads) => {
-      if (err) return res.status(500).send(err);
-      threads.forEach(thread => {
-        thread.replies = thread.replies.slice(-3);
-      });
-      res.status(200).send(threads);
+exports.post = async (req, res) => {
+  try {
+    const { text, delete_password, board } = req.body;
+    const newThread = new Thread({
+      board,
+      text,
+      delete_password,
+      created_on: new Date(),
+      bumped_on: new Date(),
+      reported: false,
+      replies: []
     });
+    const savedThread = await newThread.save();
+    res.status(200).send(savedThread);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
-exports.delete = (req, res) => {
-  const { thread_id, delete_password } = req.body;
-  Thread.findById(thread_id, (err, thread) => {
-    if (err) return res.status(500).send(err);
+exports.get = async (req, res) => {
+  try {
+    const threads = await Thread.find({ board: req.params.board })
+      .sort({ bumped_on: -1 })
+      .limit(10)
+      .populate('replies', 'text created_on')
+      .exec();
+    threads.forEach(thread => {
+      thread.replies = thread.replies.slice(-3);
+    });
+    res.status(200).send(threads);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    const { thread_id, delete_password } = req.body;
+    const thread = await Thread.findById(thread_id);
     if (!thread) return res.status(404).send('Thread not found');
     if (thread.delete_password !== delete_password) return res.status(200).send('incorrect password');
-    thread.remove((err) => {
-      if (err) return res.status(500).send(err);
-      res.status(200).send('success');
-    });
-  });
+    await thread.remove();
+    res.status(200).send('success');
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
-exports.put = (req, res) => {
-  const { thread_id } = req.body;
-  Thread.findByIdAndUpdate(thread_id, { reported: true }, (err, thread) => {
-    if (err) return res.status(500).send(err);
+exports.put = async (req, res) => {
+  try {
+    const { thread_id } = req.body;
+    const thread = await Thread.findByIdAndUpdate(thread_id, { reported: true }, { new: true });
     if (!thread) return res.status(404).send('Thread not found');
     res.status(200).send('reported');
-  });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
